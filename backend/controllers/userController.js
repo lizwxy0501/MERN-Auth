9e1b01ef987709031566729cbb66e8275194b287
lizwxy0501 <lizwxy0501@icloud.com>
2023-06-30 21:11:20 +0100

    init

---
+++ backend/controllers/userController.js
@@ -0,0 +1,81 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+
+// Auth user and set token
+// route: POST/api/users/auth
+// access: Public
+
+
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Auth user' })
+});
+
+
+// Register a new user 
+// route: POST/api/users
+// access: Public
+
+
+const registerUser = asyncHandler(async (req, res) => {
+    //get info from request
+    const {userName, email, password} = req.body;
+    //check if exists
+    const userExists = await User.findOne({email: email})
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const newUser = await User.create({
+        userName,
+        email,
+        password
+    });
+
+
+    res.status(200).json({ message: 'Register user' })
+});
+
+
+// Logout user 
+// route: POST/api/users/logout
+// access: Public
+
+
+const logoutUser = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Logout user' })
+});
+
+
+// Get user profile
+// route: GET/api/users/profile
+// access: Private
+
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Get User profile' })
+});
+
+// Update user profile
+// route: PUT/api/users/profile
+// access: Private
+
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+
+    res.status(200).json({ message: 'Update User profile' })
+});
+
+
+
+
+export {
+    authUser, 
+    registerUser,
+    logoutUser, 
+    updateUserProfile, 
+    getUserProfile
+};
