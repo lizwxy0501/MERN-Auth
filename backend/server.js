013ee0b9a236446d1f779769d0796e19652a5f54
lizwxy0501 <lizwxy0501@icloud.com>
2023-07-04 19:52:07 +0100

    add info

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.use(cookieParser());
-
 app.use(cors(
     {
         origin:['http://localhost:3000','https://mern-auth-2e1y.onrender.com']
     }
 ))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser());
 
 
 app.use('/api/users', userRoutes);
