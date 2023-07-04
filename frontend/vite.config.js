9fbdea464267c3d38768fa29712cc5371ed731b1
lizwxy0501 <lizwxy0501@icloud.com>
2023-07-04 20:01:20 +0100

    add backend info

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://mern-auth-backend-usbb.onrender.com:5000',
+        target: 'https://mern-auth-backend-usbb.onrender.com',
         changeOrigin: true
       }
     }
