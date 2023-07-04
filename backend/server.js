5f82ffaf612ca39d93adfffb6f8c9b76242dee01
lizwxy0501 <lizwxy0501@icloud.com>
2023-07-04 20:08:52 +0100

    change path info

--- backend/server.js
+++ backend/server.js
@@ -32,15 +32,15 @@ app.use(cors(
 
 app.use('/api/users', userRoutes);
 
-// if (process.env.NODE_ENV === 'production') {
-//     const __dirname = path.resolve();
-//     app.use(express.static(path.join(__dirname, 'frontend/dist')))
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
-// } else {
-//     app.get('/', (req, res) => res.send('Server is ready.'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is ready.'))
 
-// }
+}
 
 
 app.use(notFound)
