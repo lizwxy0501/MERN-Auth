07b956b4f1a85f4ddc807b5ee971db22a5b36649
lizwxy0501 <lizwxy0501@icloud.com>
2023-07-04 20:06:16 +0100

    change extended info

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 
 app.use(cookieParser());
 
